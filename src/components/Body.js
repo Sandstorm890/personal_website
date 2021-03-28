cf70d80609c15ecbc9a92755de9235a9c2c60458
zaynegoering <zaynegoering@gmail.com>
2021-03-27 22:10:49 -0700

    stub out components

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Body extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>I like 2 programmng</h2>
+            </div>
+        )
+    }
+}
+
+export default Body
