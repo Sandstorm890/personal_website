efea800c437947ff1c5af146c2bde5051b8bc80e
zaynegoering <zaynegoering@gmail.com>
2021-04-20 13:18:45 -0700

    define about section

---
+++ src/components/AboutBlurb.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AboutBlurb extends React.Component {
+    render () {
+        return (
+            <div>
+                <h3>Hello, my name is Zayne Goering</h3>
+                <p>I'm a full-stack web developer and software engineer based out of Seattle, Washington.</p>
+            </div>
+        )
+    }
+}
+
+export default AboutBlurb
